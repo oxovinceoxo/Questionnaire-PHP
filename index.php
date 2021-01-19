19842cafe68962f173d617a1a57df5d190782f70
vince <oxovinceoxo@gmail.com>
2021-01-19 12:03:54 +0100

    Signed-off-by: vince <oxovinceoxo@gmail.com> fini

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 
     <div>
       <form action="reponse.php" method="post">
+
+<!----------------------- Question 1 --------------------------------------->
         <div class="container bg-danger p-3 mb-2">
           <h1>Question 1:</h1>
             <h2>
@@ -34,14 +36,48 @@
               <option value="rose">il est rose</option>
             </select>
         </div>
+<!----------------------- Question 2 --------------------------------------->
+        <div class="container bg-primary p-3 mb-2">
+          <h1>Question 2:</h1>
+            <h2>
+              <?php
+                echo $question_tableau[1];
+              ?>
+            </h2>
 
-        <button type="submit" class=" text-center, btn btn-success m-5 " >Valider les réponses</button>
+            <input type="hidden" value="c" name="bonneReponse2"/>
+
+            <select class="form-select" name="question2">
+              <option value="a">la fée a</option>
+              <option value="b">la fée b</option>
+              <option value="c">la fée c</option>
+            </select>
+        </div>
+<!----------------------- Question 3 --------------------------------------->
+        <div class="container bg-warning p-3 mb-2">
+          <h1>Question 3:</h1>
+            <h2>
+              <?php
+                echo $question_tableau[2];
+              ?>
+            </h2>
+
+            <input type="hidden" value="ronde" name="bonneReponse3"/>
+
+            <select class="form-select" name="question3">
+              <option value="ronde">elle est ronde</option>
+              <option value="plate">elle est plate</option>
+              <option value="carree">elle est carrée</option>
+            </select>
+        </div>
+
+        <button type="submit" class=" text-center btn btn-success m-5 " >Valider les réponses</button>
       </form>
 
     </div> 
 
-    
-       
+
+
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   </body>
--- reponse.php
+++ reponse.php
@@ -1,21 +1,44 @@
 
 <?php
-    $question_tableau = ["quelle est la couleur du cheval blanc d'henri iv ?", "Quelle est la fée la plus méchante, la A, la B ou la C ?", "la terre est ronde plate ou carrée ?"]
+    $question_tableau = ["quelle est la couleur du cheval blanc d'henri iv ?", "Quelle est la fée la plus méchante, la A, la B ou la C ?", "la terre est ronde plate ou carrée ?"];
 
 
     //récupération de la bonne réponse
     $bonneReponse1 = $_POST['bonneReponse1'];
+    $bonneReponse2 = $_POST['bonneReponse2'];
+    $bonneReponse3 = $_POST['bonneReponse3'];
 
     //Récupération des questions
     $question1 = $_POST['question1'];
+    $question2 = $_POST['question2'];
+    $question3 = $_POST['question3'];
 
     //comparé les questions et réponses
+
+    //Question 1
     if($question1 == $bonneReponse1){
-        echo "<p class='alert-success p-5'>BONNE REPONSE</p>";
-        echo $tableau_question[0] ."La réponse est " . $bonneReponse1;
-        $score += 1;
+        echo "<p class='alert-success p-5'>Question 1 BONNE REPONSE</p>";
+        
     }else{
-        echo "<p class='alert-danger p-5'>MAUVAISE REPONSE</p>";
-        echo $tableau_question[0] ."La réponse est " . $bonneReponse1;
+        echo "<p class='alert-danger p-5'>Question 1 MAUVAISE REPONSE</p>";
+        
+    }
+
+    //Question 2
+    if($question2 == $bonneReponse2){
+        echo "<p class='alert-success p-5'>Question 2 BONNE REPONSE</p>";
+        
+    }else{
+        echo "<p class='alert-danger p-5'>Question 2 MAUVAISE REPONSE</p>";
+        
+    }
+    //Question 3
+    if($question3 == $bonneReponse3){
+        echo "<p class='alert-success p-5'>Question 3 BONNE REPONSE</p>";
+        
+    }else{
+        echo "<p class='alert-danger p-5'>Question 3 MAUVAISE REPONSE</p>";
+        
     }
 ?>
+
